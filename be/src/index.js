a7c7750733328fadc5b014119cbe4ac96ee18189
Viktor Shcherban <megaletterbox@gmail.com>
2023-12-05 13:45:23 +0100

    implemented basic openAI chat endpoint

--- be/src/index.js
+++ be/src/index.js
@@ -1,13 +1,56 @@
+import fetch from 'node-fetch';
+import { config } from 'dotenv';
+
+// Load environment variables from .env file in development
+if (process.env.NODE_ENV !== 'production') {
+  config();
+}
 
 export const handler = async (event) => {
-  console.log("Event: ", event);
+  // console.log("Event: ", event);
 
-// return hello world
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({message: "Hello World!"})
+  const openAiApiKey = process.env.OPENAI_API_KEY; // Load API key from environment variables
+  // console.log("openAiApiKey: ", openAiApiKey);
+
+  // const userInput = JSON.parse(event.body)?.input;
+
+  // Prepare the request to OpenAI
+  const openAiUrl = 'https://api.openai.com/v1/chat/completions';
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${openAiApiKey}`
   };
+  const data = {
+    model: "gpt-4-1106-preview",
+    messages: [
+      { "role": "system", "content": "You are a helpful assistant." },
+      { "role": "user", "content": "what is up?" }
+    ]
+  };
+
+  try {
+    const response = await fetch(openAiUrl, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(data)
+    });
+    const openAiResponse = await response.json();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(openAiResponse)
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ error: "Error calling OpenAI API" })
+    };
+  }
 };
