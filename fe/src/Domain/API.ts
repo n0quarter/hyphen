599a7c61b10c639bfb18eac193d0d625245cab90
Viktor Shcherban <megaletterbox@gmail.com>
2023-12-05 16:36:22 +0100

    updated production BE url

--- fe/src/Domain/API.ts
+++ fe/src/Domain/API.ts
@@ -2,7 +2,7 @@ import * as Network from '../Infrastructure/Network';
 
 const API_URL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:3001'
-  : 'http://hyphen.asdcode.com'; // TODO: add SSL
+  : 'https://y2g4geyzdvzj2r3vmqgkzjkmza0bropy.lambda-url.eu-central-1.on.aws/'; // TODO: add SSL
 
 export const initialize = (): void => {
   Network.initialize(API_URL);
