31b0fa2a3ad02048b64030c1e537887c97081509
Viktor Shcherban <megaletterbox@gmail.com>
2023-12-05 14:26:33 +0100

    basic layout

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-function App() {
+import styled from 'styled-components';
+import {ChatView} from "./ChatView";
+
+// FullScreenCentered component for full-screen on mobile and centered on desktop
+const FullScreenCentered = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh; // Full viewport height
+  background-color: white;
+  color: black;
+  font-size: 20px;
+  padding: 10px;
+  box-sizing: border-box; // Ensures padding is included in the element's total width and height
+
+  // Media query for desktop
+  @media (min-width: 769px) {
+    min-height: 100vh;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <p>hello world</p>
-    </div>
+    <FullScreenCentered>
+      <ChatView />
+    </FullScreenCentered>
   );
 }
 
 export default App;
+
